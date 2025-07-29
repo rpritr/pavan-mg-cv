83e761f9427f86b49ca3476c5b092f8c32c52a12
rpritr <robi@foreach-labs.si>
2025-07-29 11:55:36 +0200

    add menu

--- src/js/Components/Header/Header.js
+++ src/js/Components/Header/Header.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Logo from "./Logo/Logo";
-
+import Menu from "./Menu/Menu";
 const Header = (props) => {
     return (
         <div className={props.type}>
             <Logo type={props.type + "__logo"} />
+            <Menu items={
+                [
+                    { title: "Home", url: "#" },
+                    { title: "About", url: "#about" },
+                    { title: "Tech stack", url: "#stack" },
+                    { title: "Projects", url: "#projects" },
+                    { title: "Contact", url: "#contact" },
+                ]
+            }/>
+
         </div>
     )
 }
